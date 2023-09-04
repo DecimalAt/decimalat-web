5d963948b027b2cb95670fd160c41a4b170c62ea
Abhishek Saini <abhishek.tellius@gmail.com>
2023-09-05 02:08:24 +0530

    build fix

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import * as serviceWorker from './serviceWorker'
 import configureStore from './configureStore'
 
 //import 'typeface-ibm-plex-sans'
-import styled from 'styled-components/macro';
 
 // We use hash history because this example is going to be hosted statically.
 // Normally you would use browser history.
